35f99fb3402442dd1cac1b5a08798e5c559412d6
Foued <firas.fouad@isimg.tn>
2026-04-14 15:12:39 +0100

    Data Transformation

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -38,6 +38,23 @@ class DataTransformation:
             raise NetworkSecurityException(e,sys)
         
 
+    def get_data_transformer_object(cls)->Pipeline:
+        logging.info(
+            "Entered get_data_transformer method of transformation class"
+        )
+        try:
+                imputer :KNNImputer= KNNImputer(**DATA_TRANSFORMATION_IMPUTER_PARAMS)
+                logging.info(
+                    f"Initialise KNNiMPTER WITH {DATA_TRANSFORMATION_IMPUTER_PARAMS}"
+                )
+                processor:Pipeline=Pipeline([("Imputer",imputer)])
+                return processor
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+
+
+
 
 
 
@@ -61,6 +78,29 @@ class DataTransformation:
             input_feature_test_df=test_df.drop(columns=[TARGET_COLUMN],axis=1)
             target_feature_test_df=test_df[TARGET_COLUMN]
             target_feature_test_df=target_feature_test_df.replace(-1,0)
+            preprocessor=self.get_data_transformer_object()
+            preprocessor_object=preprocessor.fit(input_feature_train_df)
+            transformed_input_train_feature=preprocessor_object.transform(input_feature_train_df)
+            transformed_input_test_feature=preprocessor_object.transform(input_feature_test_df)
+
+            train_arr = np.c_[transformed_input_train_feature,np.array(target_feature_train_df)]
+            test_arr = np.c_[transformed_input_test_feature,np.array(target_feature_test_df)]
+            
+
+            #save numpy array data 
+            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path,array=train_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path,array=test_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_object_file_path,preprocessor_object)
+
+            #Prepare artifact
+
+            data_validation_artifact=DataTransformationArtifact(
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path        
+                )
+            return data_validation_artifact
+
 
 
 
